1e22c6229d57fd86b3efc99181c0e205b2e49160
Mohamed Zidane <mohamedzidane11@gmail.com>
2023-02-11 14:35:44 +0200

    create polls urls and index view page

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def index(request):
+    return HttpResponse("awesome job guys, this is the index page, of our polls app")
+
