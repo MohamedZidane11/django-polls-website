2169854c42e103cb848f8cf51ea6a2d3607c41a2
Mohamed Zidane <mohamedzidane11@gmail.com>
2023-02-11 15:06:35 +0200

    creating detail, results, vote view functions

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -5,3 +5,14 @@ from django.http import HttpResponse
 def index(request):
     return HttpResponse("awesome job guys, this is the index page, of our polls app")
 
+
+def detail(request, question_id):
+    return HttpResponse(f"this is the detail view of the question: {question_id}")
+
+
+def results(request, question_id):
+    return HttpResponse(f"this is the result of the question: {question_id}")
+
+
+def vote(request, question_id):
+    return HttpResponse(f"votes on question: {question_id}")
