c91dd20c27b4cb1c937d308921c08d80d38fc5f8
Mohamed Zidane <mohamedzidane11@gmail.com>
2023-02-11 15:29:43 +0200

    adding the detail, results, vote urls to polls app

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name = "index"), # http://127.0.0.1:8000/polls/
+    path('', views.index, name="index"), # http://127.0.0.1:8000/polls/
+    path('<int:question_id>/', views.detail, name="detail"), # http://127.0.0.1:8000/polls/1
+    path('<int:question_id>/results/', views.results, name="results"), # http://127.0.0.1:8000/polls/1/results
+    path('<int:question_id>/vote/', views.vote, name="vote"), # http://127.0.0.1:8000/polls/1/vote
 ]
