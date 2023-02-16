fa9988b1ab2e4414a976519fb56bbcc06f5d5027
Mohamed Zidane <mohamedzidane11@gmail.com>
2023-02-16 17:01:34 +0200

    creating our polls index.html and link it to polls view

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Question
 
 # Create your views here.
 def index(request):
-    return HttpResponse("awesome job guys, this is the index page, of our polls app")
+    latest_questions = Question.objects.order_by('-publish_date')[:5]
+    context = {'latest_questions': latest_questions}
+    return render(request, 'polls/index.html', context)
+    # return HttpResponse("awesome job guys, this is the index page, of our polls app")
 
 
 def detail(request, question_id):
