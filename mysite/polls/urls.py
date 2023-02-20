902062403ea1850cc0de6a77d2a8994983d7257c
Mohamed Zidane <mohamedzidane11@gmail.com>
2023-02-20 09:55:24 +0200

    modifying polls url, index.html, put form in detail.html

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 
+app_name = 'polls'
 urlpatterns = [
     path('', views.index, name="index"), # http://127.0.0.1:8000/polls/
     path('<int:question_id>/', views.detail, name="detail"), # http://127.0.0.1:8000/polls/1
